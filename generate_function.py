52feec9be47b2afdf7a665ddaa483637ddc11847
Raffaelbdl <raffaelbdl@gmail.com>
2022-02-23 15:12:46 +0100

    add fn

--- generate_function.py
+++ generate_function.py
@@ -4,25 +4,6 @@ import matplotlib.pyplot as plt
 
 from typing import Tuple
 
-# x = np.arange(-5.0, 5.01, 1)
-# y = np.arange(-5.0, 5.01, 1)
-# xx, yy = np.meshgrid(x, y)
-# z = np.random.uniform(-0.1, 0.1, size=xx.shape)
-# print(z.shape)
-
-# f = interpolate.interp2d(x, y, z)
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# xnew = np.arange(-5.0, 5.01, 1e-1)
-# ynew = np.arange(-5.0, 5.01, 1e-1)
-# xxnew, yynew = np.meshgrid(xnew, ynew)
-# print(xnew, ynew)
-# znew = f(xnew, ynew)
-# ax.scatter3D(0.1, 0.1, f(0.1, 0.1), s=20, c='r', marker='o')
-# ax.plot_wireframe(xxnew, yynew, znew)
-# plt.show()
-
 def generate_random_interpolation(min: float, max: float, step: float) -> interpolate.interp2d:
 
     x = np.arange(min, max, step)
