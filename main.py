4dd498e6c44a16290e0443956444ac256c104041
Raffaelbdl <raffaelbdl@gmail.com>
2022-03-09 16:53:20 +0100

    add meteo2

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from simulations import plots
 ### PATH ###
 path_to_stations = "./2D_coupe/stations_2D_coupe"
 simulation_name1 = "2D_coupe_meteo1"
-simulation_name2 = "2D_coupe_hors_sol"
+simulation_name2 = "2D_coupe_meteo2"
+simulation_name3 = "2D_coupe_hors_sol"
 
 ### GET RELIEF ###
 x_relief, y_relief = plots._relief()
@@ -56,43 +57,55 @@ x22, signals_l22 = plots._scatter_stations_intensity(
     path_to_stations=path_to_stations,
     norm="L2"
 )
+x13, signals_inf3 = plots._scatter_stations_intensity(
+    simulation=simulation_name3,
+    path_to_stations=path_to_stations,
+    norm="Inf"
+)
+x23, signals_l23 = plots._scatter_stations_intensity(
+    simulation=simulation_name3,
+    path_to_stations=path_to_stations,
+    norm="L2"
+)
 
 if __name__ == "__main__":
 
-    # fig, ax1 = plt.subplots()
-    # ax2 = ax1.twinx()
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
 
-    # ax1.plot(
-    #     x_relief, y_relief, 
-    #     c=relief_color, alpha=relief_alpha,
-    # )
-    # ax1.scatter(
-    #     x_stations, y_stations, 
-    #     c=stations_color, s=stations_size,   
-    # )
-    # ax1.scatter(
-    #     x_source, y_source, 
-    #     c=source_color, s=source_size,
-    # )
-    # ax1.set_xlabel("X (m)")
-    # ax1.set_ylabel("Altitude (m)")
-    # ax1.legend(["Relief", "Stations", "Source"], loc='upper left')
+    ax1.plot(
+        x_relief, y_relief, 
+        c=relief_color, alpha=relief_alpha,
+    )
+    ax1.scatter(
+        x_stations, y_stations, 
+        c=stations_color, s=stations_size,   
+    )
+    ax1.scatter(
+        x_source, y_source, 
+        c=source_color, s=source_size,
+    )
+    ax1.set_xlabel("X (m)")
+    ax1.set_ylabel("Altitude (m)")
+    ax1.legend(["Relief", "Stations", "Source"], loc='upper left')
 
-    # norm = "L2"
-    # if norm == "Inf":
-    #     ax2.scatter(x1, signals_inf, c='cyan')
-    #     ax2.scatter(x12, signals_inf2, c='pink')
-    #     ax2.legend(["Norme Infinie 1", "Norme Infinie 2"], loc='upper right')
-    # elif norm == "L2":
-    #     ax2.scatter(x2, signals_l2, c='cyan')
-    #     ax2.scatter(x22, signals_l22, c='pink')
-    #     ax2.legend(["Norme 2 1", "Norme 2 2"], loc='upper right')
-    # ax2.set_ylabel("Surpression (Pa)")
+    norm = "Inf"
+    if norm == "Inf":
+        ax2.scatter(x1, signals_inf, c='k')
+        ax2.scatter(x12, signals_inf2, c='orange')
+        ax2.scatter(x13, signals_inf3, c='b')
+        ax2.legend(["Norme Infinie Meteo", "Norme Infinie Meteo", "Norme Infinie Sans Vent"], loc='upper right')
+    elif norm == "L2":
+        ax2.scatter(x2, signals_l2, c='cyan')
+        ax2.scatter(x22, signals_l22, c='pink')
+        ax2.scatter(x23, signals_l23, c='yellow')
+        ax2.legend(["Norme 2 1", "Norme 2 2", "Norme 2 3"], loc='upper right')
+    ax2.set_ylabel("Surpression (Pa)")
 
     # ax2.scatter(per_pos, per_pos_y, marker="x")
     
-    plots.plot_station_signal("2D_coupe_hors_sol", 36, c='r')
-    plots.plot_station_signal("2D_coupe_meteo1", 36, c='g')
+    # plots.plot_station_signal("2D_coupe_hors_sol", 36, c='r')
+    # plots.plot_station_signal("2D_coupe_meteo1", 36, c='g')
 
 
     plt.show()
