3ceecd2f892c3ceb6221def539a7986a3cf784b5
Raffaelbdl <raffaelbdl@gmail.com>
2022-03-30 11:33:47 +0200

    generate 3D

---
+++ generate_stations_file.py
@@ -0,0 +1,25 @@
+import numpy as np
+import csv
+
+# a = np.loadtxt("./3D_coupe/3D_pas_100m_meteo0_corr/TE/STATION_NOM")
+# print(a)
+
+stations = []
+
+with open("./3D_coupe/3D_pas_100m_meteo0_corr/TE/STATION_NOM", "r") as f:
+    a = csv.reader(f, delimiter=' ')
+    for row in a:
+        print(row)
+        if row[0][:2] == 'ST':
+            stations.append(
+                [
+                    row[1],
+                    row[2],
+                    row[3]
+                ]
+            )
+
+stations = np.array(stations, dtype=np.float32)
+print(stations)
+
+np.savetxt("./3D_coupe/stations_3D_coupe", stations)
