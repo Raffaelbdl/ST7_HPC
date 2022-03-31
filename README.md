df17c6b292efedb27b345c568c4547b7b990d344
Raffaelbdl <raffaelbdl@gmail.com>
2022-03-31 13:27:56 +0200

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ST7_HPC
+
+Group project by Julie, Veschambre, Mickael Levy, Nicolas Ibanez, Raffael Bolla Di Lorenzo
+In collaboration with The French Alternative Energies and Atomic Energy Commission and CentraleSupélec
+
+The goal of this project is to detect an explosion in a given location.
+
+The results of the simulations are available in *2D_coupe* and *3D_coupe*.
+To display the results, change the config in `main2D.py` or `main3D.py`.
--- make_gif.py
+++ make_gif.py
@@ -4,36 +4,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import imageio
 from tqdm import tqdm
+
 from utils import generate_stations, plots
 
 
 ### 2D GIF ### 
 
-# stations_coords = generate_stations.make_stations()
-# init_stations_y = 9e3 * np.ones_like(stations_coords[..., 1]) 
-# relief = plots._relief()
+stations_coords = generate_stations.make_stations()
+init_stations_y = 9e3 * np.ones_like(stations_coords[..., 1]) 
+relief = plots._relief()
+x_source = 19382
+y_source = 4290
 
-# num_ite = 100
-# step = 100
-# for k in range(num_ite):
-#     stations_y = np.maximum(stations_coords[..., 1], init_stations_y - step * k)
-#     fig, ax = plt.subplots()
-#     ax.plot(relief[0], relief[1], c='k', alpha=0.4)
-#     ax.scatter(stations_coords[..., 0], stations_y, c='g', s=25)
-#     ax.set_ylim([1000, 9000])
-#     plt.savefig(f"./gifs/png_{k}")
-#     plt.close()
+num_ite = 100
+step = 100
+for k in tqdm(range(num_ite)):
+    stations_y = np.maximum(stations_coords[..., 1], init_stations_y - step * k)
+    fig, ax = plt.subplots()
+    ax.plot(relief[0], relief[1], c='k', alpha=0.4)
+    ax.scatter(stations_coords[..., 0], stations_y, c='g', s=25)
+    ax.scatter(x_source, y_source, c='r', s=100)
+    ax.set_ylim([1000, 9000])
+    plt.savefig(f"./gifs_2D/{k}", dpi=200)
+    plt.close()
 
-### 3D GIF ###
+listdir = [str(a) + ".png" for a in sorted(int(c[:-4]) for c in os.listdir("./gifs_2D/"))]
+iterator = tqdm(listdir)
+with imageio.get_writer("gif_2D.gif", mode='I') as writer:
+    for filename in iterator:
+        image = imageio.imread(os.path.join("./gifs_2D/", filename))
+        writer.append_data(image)
+
+# ### 3D GIF ###
 
 # stations_coords = generate_stations.make_stations_3D()
 # init_stations_z = 9e3 * np.ones_like(stations_coords[..., 2])
+# x_source = 14412
+# y_source = 16624
+# z_source = 1900
 
 # num_ite = 200
 # step = 50
 # step_az = 5
 # azimuts = np.linspace(5 * 36, 261, num=num_ite)
-# for k in range(num_ite):
+# for k in tqdm(range(num_ite)):
 #     stations_z = np.maximum(stations_coords[..., 2], init_stations_z - step * k)  
 
 #     fig, ax, _ = plots.plot_relief3D(cmap='terrain', antialiased=True)
@@ -43,6 +57,7 @@ from utils import generate_stations, plots
 #         stations_coords[..., 1], 
 #         stations_z,
 #         c='g', s=5)
+#     ax.scatter3D(x_source, y_source, z_source, c='r', s=100)
 #     ax.set_zlim([1000, 9000])
 #     plt.savefig(f"./gifs_3D/{k}", dpi=100)
 #     plt.close()
@@ -56,9 +71,3 @@ from utils import generate_stations, plots
 #         writer.append_data(image)
 
 
-listdir = ["png_" + str(a) + ".png" for a in sorted(int(c[:-4][4:]) for c in os.listdir("./gifs_2D/"))]
-iterator = tqdm(listdir)
-with imageio.get_writer("gif_2D.gif", mode='I') as writer:
-    for filename in iterator:
-        image = imageio.imread(os.path.join("./gifs_2D/", filename))
-        writer.append_data(image)
